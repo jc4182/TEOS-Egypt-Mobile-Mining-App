2a6f917ed61d77e64e2ed9fdb34b359e21898bd0
KOSASIH <kosasihg88@gmail.com>
2025-05-25 13:27:37 +0700

    Create XRView.js

---
+++ src/components/XRView.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { XR } from 'react-native-xr'; // Hypothetical XR library
+
+const XRView = () => {
+  return (
+    <View>
+      <XR>
+        {/* XR content here */}
+      </XR>
+    </View>
+  );
+};
+
+export default XRView;
